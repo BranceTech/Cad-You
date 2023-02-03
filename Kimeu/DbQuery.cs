7010b2bae56838da0e60f8e6823d4157e2156662
BranceTech <59289031+BranceTech@users.noreply.github.com>
2023-02-03 14:25:07 +0300

    Add files via upload

---
+++ Kimeu/DbQuery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Data;
+using System.Security.RightsManagement;
+
+namespace sgs
+{
+	internal class DbQuery
+	{
+		private SQLiteDataAdapter sda;
+		public DataTable table;
+		private SQLiteCommand cmd;
+		private SQLiteDataReader read;
+		SQLiteConnection connection = new SQLiteConnection("DataSource=SGS.db");
+		public void Co()
+		{
+			if (connection.State == System.Data.ConnectionState.Closed)
+			{
+				connection.Open();
+			}
+		}
+		public void Disco()
+		{
+			if (connection.State == System.Data.ConnectionState.Open)
+			{
+				connection.Close();
+			}
+		}
+		
+		public int Query(string query, int i)
+		{
+			//try
+			{
+
+				Co();
+				sda = new SQLiteDataAdapter(query, connection);
+				DataTable dt = new DataTable();
+				table = new DataTable();
+				sda.Fill(table);
+				Disco();
+				if (table.Rows.Count > 0)
+					i = 1;
+
+			}
+			return i;
+		}
+
+		internal void Query(string query2)
+		{
+			Co();
+			sda = new SQLiteDataAdapter(query2, connection);
+			DataTable dt = new DataTable();
+			table = new DataTable();
+			sda.Fill(table);
+			Disco();
+
+		}
+	}
+}
---
+++ Kimeu/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using sgs.Views;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sgs
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			//DataContext = new Home();
+			InitializeComponent();
+		}
+		private SQLiteDataAdapter sda;
+		public DataTable table;
+		private SQLiteCommand cmd;
+		private SQLiteDataReader read;
+		SQLiteConnection connection = new SQLiteConnection("DataSource=SGS.db");
+
+
+		private void Home_Clicked(object sender, RoutedEventArgs e)
+		{
+			//DataContext = new Home();
+		}
+
+		private void Student_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new student();
+		}
+
+		private void Teacher_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new Teacher();
+		}
+
+		private void Subject_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new Subject();
+		}
+
+		private void Class_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new Class();
+		}
+
+		private void Note_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new Note();
+		}
+
+		private void View_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new View();
+		}
+		private void Grade_Clicked(object sender, RoutedEventArgs e)
+		{
+			DataContext = new Grades();
+		}
+
+		
+
+		private void btnEditGrade_Click(object sender, RoutedEventArgs e)
+		{
+			DataContext = new EditGrades();
+		}
+	}
+}
